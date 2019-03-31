8fe32849589121114f2dc34825ebf35fc5a3b23d
alvinloh@tuaspower.com.sg <M8S24Vds>
2019-03-31 15:46:13 +0800

    Changed quote model and changed tag saved to upserts

--- models/quote.js
+++ models/quote.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
+var timeZone = require('mongoose-timezone');
 var Schema = mongoose.Schema;
 
 var QuoteSchema = new Schema({
     message: {type:String , required: true},
+    author: {type:String, required: true},
+    createdon: {type:Date},
     tags: [{type:String}],
+    numoflikes: {type:Number, required:true},
     submitter: {type:String, required: true}
 });
+
+QuoteSchema.plugin(timeZone);
 
 module.exports = mongoose.model('Quote' , QuoteSchema);
--- writeToCloudDB/index.js
+++ writeToCloudDB/index.js
@@ -6,80 +6,98 @@ var Tag = require('../models/tag');
 
 module.exports = function (context, req) {
     
-    var connString = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@" +  process.env.DB_HOST + "/" + process.env.DB_NAME + "?retryWrites=true";
+    
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-    mongoose.connect(connString, {useNewUrlParser: true});
+    var payLoad = {};
 
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
+    if (req.body){
+        payLoad = req.body;
 
-    db.on('open', function(err){
-        console.log('Connected to Mongo DB');
-
-
-        var newQuote = new Quote({
-            message: 'Test Message',
-            tags: ['tag1', 'tag2'],
-            submitter: 'Alvin'
-        });
-        
-
-        // If there are tags, insert the tags first
-        if (newQuote.tags.length > 0){
-
-            // Loop through the newQuote
-            var tagsArray = newQuote.tags;
-
-            async.forEachOf(tagsArray , function( value , key , callback){
-                var newTag = new Tag({
-                    name: value
-                });
-                newTag.save(function(err, result){
-                    console.log("Saved Tag : " + result.id);
-                    callback();
-                });
-            },function(err){
-                if (err) {console.log("Error writing tags : " + err);}
-
-                // We are done inserting tags,  let's insert the main quote
-                newQuote.save(function(err , result){
-                    console.log("Message saved : " + result.id);
-                    mongoose.connection.close();
-
-                    context.res = {
-                        status: 200,
-                        headers: {
-                              'Content-Type': 'text/html'
-                            },
-                        body: "Quote Saved with ID " + result.id
-                      };
-                      context.done();
-
-                  
-                });
-
-            });
-
+        // Check at least the message is available
+        if ((!payLoad.message) || (payLoad.message == '')){
+            context.res = {
+                status: 400,
+                body: "Please enter the message for the quote"
+            }
+            context.done();
         }
 
-    });
+
+        // Proceed to parse and save the message
+        var connString = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@" +  process.env.DB_HOST + "/" + process.env.DB_NAME + "?retryWrites=true";
+
+        mongoose.connect(connString, {useNewUrlParser: true});
+
+        var db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error:'));
+
+        db.on('open', function(err){
+            console.log('Connected to Mongo DB');
 
 
+            var newQuote = new Quote({
+                message: payLoad.message,
+                author: payLoad.author ? payLoad.author : 'Anonymous',
+                createdon: Date.now(),
+                tags: payLoad.tags,
+                numoflikes: 0,
+                submitter: payLoad.submitter ? payLoad.submitter : 'Anonymous'
+            });
+            
 
-    //context.log('JavaScript HTTP trigger function processed a request.');
+            // If there are tags, insert the tags first
+            if (newQuote.tags.length > 0){
 
-    //if (req.query.name || (req.body && req.body.name)) {
-    //    context.res = {
-            // status: 200, /* Defaults to 200 */
-    //        body: "Hello " + (req.query.name || req.body.name)
-    //    };
-    //}
-    //else {
-    //    context.res = {
-    //        status: 400,
-     //       body: "Please pass a name on the query string or in the request body"
-     //   };
-    //} 
+                // Loop through the newQuote
+                var tagsArray = newQuote.tags;
+
+                async.forEachOf(tagsArray , function( value , key , callback){
+                    var newTag = new Tag({
+                        name: value
+                    });
+
+                    Tag.findOneAndUpdate(
+                        {name: value},
+                        newTag,
+                        {upsert: true, new: true, runValidators: true},
+                        function(err, result){
+                            console.log("Processed Tag : " + value);
+                            callback();
+                        }
+                    );
+
+                },function(err){
+                    if (err) {console.log("Error writing tags : " + err);}
+
+                    // We are done inserting tags,  let's insert the main quote
+                    newQuote.save(function(err , result){
+                        console.log("Message saved : " + result.id);
+                        mongoose.connection.close();
+
+                        context.res = {
+                            status: 200,
+                            body: "Quote Saved with ID " + result.id
+                        };
+                        context.done();
+
+                    });
+                });
+            }
+        });
+
+    } // Close if req.body == true
+
+    else{
+        // Just return a 404
+        context.res = {
+            status: 400, /* Defaults to 200 */
+            body: "Please input a valid Quote payload"
+        };
+        context.done();
+    }
+
+
 
 
 };
